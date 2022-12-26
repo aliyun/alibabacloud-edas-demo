d780fd15a57bea6410248c14a3517abb25ee43d6
alickreborn <alickreborn@gmail.com>
2022-12-26 21:35:09 +0800

    Eureka/Nacos 双注册 Demo

---
+++ edas-demo/dubbo-demo/edas-dubbo-demo/springcloud-dubbo-starter/src/main/java/com/alibaba/edas/IHelloService.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2022-present Alibaba Group
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.alibaba.edas;
+
+public interface IHelloService {
+
+    String sayHello(String str);
+}
